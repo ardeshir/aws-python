d43e5d4d8a5e7d0e0ca3c13db03cfc446b58a4e8
ardeshir <ardeshir.org@gmail.com>
2018-08-02 08:41:08 -0500

    adding doc string

--- 01-webmaster/webmaster/webmaster.py
+++ 01-webmaster/webmaster/webmaster.py
@@ -1,6 +1,7 @@
 import boto3
 import click
 from botocore.exceptions import ClientError
+
 #import sys
 # click replaced it
 # print(sys.argv)
@@ -10,26 +11,26 @@ s3 = session.resource('s3')
 
 @click.group()
 def cli():
-    "Webotron deploys websites to AWS"
+    """Webmaster deploys websites to AWS"""
     pass
 
 @cli.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"
+    """List all s3 buckets"""
     for bucket in s3.buckets.all():
         print(bucket)
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    "List object in an s3 bucket"
+    """List objects in an s3 bucket"""
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
 
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-    "Create and configure S3 bucket"
+    """Create and configure S3 bucket"""
     s3_bucket = None
     
     try:
